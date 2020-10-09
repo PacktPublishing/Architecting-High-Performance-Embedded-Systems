321a53bddc377bc0a68de81a8a52d1c7022382e6
Jim Ledin <jim@ledin.com>
2020-10-08 17:35:19 -0700

    Working on chapter 5

--- Chapter05/src/ArtyAdderHLS.cpp
+++ Chapter05/src/ArtyAdderHLS.cpp
@@ -1,10 +1,10 @@
 #include <ap_int.h>
 
-void Hello2(ap_uint<4> a, ap_uint<4> b,
-		ap_uint<4> *sum, ap_uint<1> *c_out)
+void ArtyAdder4HLS(ap_uint<4> a, ap_uint<4> b,
+        ap_uint<4> *sum, ap_uint<1> *c_out)
 {
-	unsigned sum5 = a + b;
+    unsigned sum5 = a + b;
 
-	*sum = sum5;
-	*c_out = sum5 >> 4;
+    *sum = sum5;
+    *c_out = sum5 >> 4;
 }
