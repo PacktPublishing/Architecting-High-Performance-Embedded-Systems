7b43b49a353df092c700e3834466c1e66257c41d
Jim Ledin <jim@ledin.com>
2021-01-16 13:21:31 -0800

    Working on chapter 5

--- Chapter05/src/set_drive_letter.bat
+++ Chapter05/src/set_drive_letter.bat
@@ -1,5 +1,8 @@
+@ECHO OFF
 REM Use this batch file to set a drive letter for the project base directory.
 REM This file assumes the GitHub repository has been cloned to C:\Projects\
 REM This is necessary because Vivado complains about a 260 character path limit.
 
-net use Z: \\localhost\c$\Projects\Architecting-High-Performance-Embedded-Systems\Chapter05\src
+SUBST Z: /D > NUL
+SUBST Z: C:\Projects\Architecting-High-Performance-Embedded-Systems\Chapter05\src
+SUBST
